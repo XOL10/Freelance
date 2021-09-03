b5920f6068bef4722384b9742f91ebd0747e6365
Juan Pablo Cabellos Aguilar <juanpablo@MacBook-de-Juan-Pablo.local>
2021-09-03 10:17:39 -0500

    Added lat to SeasonDisplay

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,27 +2,21 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
-    
-    constructor(props){
-        super(props);
 
-        this.state = { lat: null, errorMessage: ''};
+    state = { lat: null, errorMessage: ''}
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
-    };
+    }
     
     render() {
         if (this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>;
         }else if (this.state.lat && !this.state.errorMessage){
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }else{
             return <div>Loading...</div>;
         }
